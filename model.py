255e777d8a52731d2e7a4cd7229581149668538a
HBaena <a.hernandezbaena@ugto.mx>
2019-11-19 23:24:15 -0600

    Create User table from SQLAlchemy model.

--- model.py
+++ model.py
@@ -1,2 +1,14 @@
 from flask_sqlalchemy import SQLAlchemy
 from settings import app
+
+db = SQLAlchemy(app)
+
+
+class User(db.Model):
+    """docstring for User"""
+    user_id = db.Column(db.Integer, nullable=False, primary_key=True)
+    last_name = db.Column(db.String(25), nullable=False)
+    name = db.Column(db.String(20), nullable=False)
+    email = db.Column(db.String(30), nullable=False)
+    phone = db.Column(db.String(10), nullable=False)
+    password = db.Column(db.String(25), nullable=False)
