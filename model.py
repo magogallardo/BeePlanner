80d486b8e43d573510f48688b20d993b77ef819e
MoyMontes <mm.montesvillanueva@ugto.mx>
2019-11-22 11:38:48 -0600

    actualice la implementacion de User, agrgando dos columnas

--- model.py
+++ model.py
@@ -1,7 +1,6 @@
 from settings import db
 
 
-
 class User(db.Model):
     """docstring for User"""
     user_id = db.Column(db.Integer, nullable=False, primary_key=True)
@@ -10,6 +9,8 @@ class User(db.Model):
     email = db.Column(db.String(30), nullable=False)
     phone = db.Column(db.String(10), nullable=False)
     password = db.Column(db.String(25), nullable=False)
+    create_date = db.Column(db.DateTime, nullable=False)
+    modified_date = db.Column(db.DateTim, nullable=False)
     # db.relationship must be in the parent table
     activity = db.relationship('Activity', backref='user')
 
