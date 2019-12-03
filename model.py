bdcbb8f145c23747be82d23795b538a73e65e549
MoyMontes <45276668+MoyMontes@users.noreply.github.com>
2019-12-03 16:21:59 -0600

    update model.py

--- model.py
+++ model.py
@@ -5,14 +5,12 @@ class User(db.Model):
     """docstring for User"""
 
     __tablename__ = "User"
-    user_id = db.Column(db.Integer, nullable=False, primary_key=True)
-    last_name = db.Column(db.String(25), nullable=False)
-    name = db.Column(db.String(20), nullable=False)
+    username = db.Column(db.String(30), nullable=False, primary_key=True)
     email = db.Column(db.String(30), nullable=False)
-    phone = db.Column(db.String(10), nullable=False)
-    password = db.Column(db.String(25), nullable=False)
-    create_date = db.Column(db.DateTime, nullable=False)
-    modified_date = db.Column(db.DateTime, nullable=False)
+    name = db.Column(db.String(30), nullable=False)
+    last_name = db.Column(db.String(30), nullable=False)
+    phone = db.Column(db.String(12), nullable=False)
+    password = db.Column(db.String(64), nullable=False)
     # db.relationship must      be in the parent table
     activity = db.relationship('Activity', backref='User')
 
@@ -21,69 +19,15 @@ class Activity(db.Model):
     """docstring for Activity"""
 
     __tablename__ = "Activity"
-    activity_id = db.Column(db.Integer, nullable=False, primary_key=True)
-    name = db.Column(db.String(25), nullable=False)
+    activity_id = db.Column(db.Integer, nullable=True, primary_key=True)
+    description = db.Column(db.String(300), nullable=False)
+    title = db.Column(db.String(30), nullable=False)
     priority = db.Column(db.Integer, nullable=False)
-    is_subject = db.Column(db.Boolean, nullable=False)
-    user_id = db.Column(db.Integer, db.ForeignKey(
-        'User.user_id'), nullable=False)
-    start_time = db.Column(db.DateTime, nullable=False)
-    finish_time = db.Column(db.DateTime, nullable=False)
-    monday = db.Column(db.Boolean, nullable=False)
-    tuesday = db.Column(db.Boolean, nullable=False)
-    wednesday = db.Column(db.Boolean, nullable=False)
-    thursday = db.Column(db.Boolean, nullable=False)
-    friday = db.Column(db.Boolean, nullable=False)
-    saturday = db.Column(db.Boolean, nullable=False)
-    sunday = db.Column(db.Boolean, nullable=False)
-    create_date = db.Column(db.DateTime, nullable=False)
-    modified_date = db.Column(db.DateTime, nullable=False)
-    # db.relationship must be in the parent table
-    task = db.relationship('Task', backref='Activity')
-
-
-class Task(db.Model):
-    """docstring for Task"""
-
-    __tablename__ = "Task"
-    task_id = db.Column(db.Integer, nullable=False, primary_key=True)
-    name = db.Column(db.String(20), nullable=False,)
-    description = db.Column(db.String(30), nullable=False)
-    progress = db.Column(db.Integer, nullable=False)
-    due_date = db.Column(db.DateTime, nullable=False)
-    create_date = db.Column(db.DateTime, nullable=False)
-    modified_date = db.Column(db.DateTime, nullable=False)
-    activity_id = db.Column(db.Integer, db.ForeignKey(
-        'Activity.activity_id'), nullable=False)
-
-
-class ListTask(db.Model):
-    """docstring for ListTask"""
-
-    __tablename__ = "ListTask"
-    task_id = db.Column(db.Integer, db.ForeignKey(
-        'task.task_id'), nullable=False, primary_key=True)
-    activity_id = db.Column(db.Integer, db.ForeignKey(
-        'task.activity_id'), nullable=False, primary_key=True)
-    list_item = db.Column(db.String(20), nullable=False)
-    # TODO: PRIMARY KEY(task_id, list_item)
-    create_date = db.Column(db.DateTime, nullable=False)
-    modified_date = db.Column(db.DateTime, nullable=False)
-
-
-class Reminder(db.Model):
-    """docstring for Reminder"""
-
-    __tablename__ = "Reminder"
-    reminder_id = db.Column(db.Integer, nullable=False, primary_key=True)
-    name = db.Column(db.String(25), nullable=False,)
-    description = db.Column(db.String(25), nullable=False,)
-    reminder_date = db.Column(db.DateTime, nullable=False,)
-    is_all_day = db.Column(db.Boolean, nullable=False)
-    create_date = db.Column(db.DateTime, nullable=False)
-    modified_date = db.Column(db.DateTime, nullable=False)
-    activity_id = db.Column(db.Integer, db.ForeignKey(
-        'Activity.activity_id'), nullable=False)
+    location = db.Column(db.String(50), nullable=False)
+    username = db.Column(db.Integer, db.ForeignKey(
+        'User.username'), nullable=False)
+    # db.relationship must      be in the parent table
+    note = db.relationship('Note', backref='Activity')
 
 
 class Note(db.Model):
@@ -91,9 +35,24 @@ class Note(db.Model):
 
     __tablename__ = "Note"
     note_id = db.Column(db.Integer, nullable=False, primary_key=True)
-    name = db.Column(db.String(25), nullable=False)
-    description = db.Column(db.String(30), nullable=False, )
-    create_date = db.Column(db.DateTime, nullable=False)
-    modified_date = db.Column(db.DateTime, nullable=False)
-    activity_id = db.Column(db.Integer, db.sForeignKey(
+    content = db.Column(db.String(25), nullable=False)
+    priority = db.Column(db.String(30), nullable=False, )
+    due_date = db.Column(db.DateTime, nullable=False)
+    creationt_date = db.Column(db.DateTime, nullable=False)
+    username = db.Column(db.Integer, db.ForeignKey(
+        'User.username'), nullable=False)
+    activity_id = db.Column(db.Integer, db.ForeignKey(
+        'Activity.activity_id'), nullable=False)
+
+
+class Schedule(db.Model):
+    """docstring for Schedule"""
+
+    __tablename__ = "Schedule"
+    monday = db.Column(db.String(25), nullable=False)
+    tuesday = db.Column(db.String(25), nullable=False)
+    wednesday = db.Column(db.String(25), nullable=False)
+    thursday = db.Column(db.String(25), nullable=False)
+    friday = db.Column(db.String(25), nullable=False)
+    activity_id = db.Column(db.Integer, db.ForeignKey(
         'Activity.activity_id'), nullable=False)
