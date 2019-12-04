648a3d58061fc1696e60a55bd2bd36de297a15b8
HBaena <a.hernandezbaena@ugto.mx>
2019-12-03 19:31:56 -0600

    Adding templates for modals, pop-up Adding views for schedule, register Modifing index Adding some routes within app

--- app.py
+++ app.py
@@ -1,13 +1,27 @@
-from flask import render_template, session, redirect, url_for
+from flask import render_template, session, redirect, url_for, request
 from settings import app
+from random import choice
 # import os
+modals_colors = (
+    'red lighten-3',
+    'amber lighten-3',
+    'deep-orange accent-1',
+    'blue-grey lighten-1',
+    'amber accent-3',
+    'lime lighten-2',
+    'teal accent-3',
+    'indigo lighten-3',
+    'purple accent-1',
+    'red lighten-2',
+    'light-green lighten-2'
+)
 
 
-@app.route('/')
-@app.route('/index')
-@app.route('/home')
-def home():
-    session['user_name'] = ''
+def get_random_color():
+    return choice(modals_colors)
+
+
+def logged_args():
     if 'user_name' not in session:
         status_log = 'Login'
         icon_log = 'account_circle'
@@ -15,29 +29,45 @@ def home():
 
         status_account = 'Sign up'
         icon_account = 'person_add'
-        redirect_accoutn = 'register'
-
+        redirect_account = 'register'
     else:
         status_account = 'Profile'
         icon_account = 'settings'
-        redirect_accoutn = 'profile'
+        redirect_account = 'profile'
 
         status_log = 'Logout'
         icon_log = 'exit_to_app'
         redirect_log = 'logout'
 
-    return render_template('index.html', title='HOME',
-                           status_log=status_log, icon_log=icon_log,
-                           redirect_log=redirect_log,
-                           status_account=status_account,
-                           icon_account=icon_account,
-                           redirect_accoutn=redirect_accoutn
-                           )
+    return locals()
 
 
-@app.route('/login')
+@app.route('/schedule')
+def schedule():
+    return render_template('schedule.html')
+
+
+@app.route('/')
+@app.route('/index')
+@app.route('/home')
+# @logged_args
+def home():
+    # session['user_name'] = ''
+    kwargs = logged_args()
+    redirect_page = 'schedule.html' if 'user_name' in session else 'index.html'
+    return render_template(redirect_page, title='HOME', **kwargs)
+
+
+@app.route('/login', mathods=['GET'])
 def login():
-    return render_template('login.html', title='LOGIN')
+    kwargs = logged_args()
+    return render_template('login.html', title='LOGIN', **kwargs)
+
+
+@app.route('/register')
+def register():
+    kwargs = logged_args()
+    return render_template('register.html', title='REGISTER', **kwargs)
 
 
 @app.route('/logout')
