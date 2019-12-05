743ac7ffd150a69047bb539925c473b9106ae510
HBaena <a.hernandezbaena@ugto.mx>
2019-12-05 10:25:43 -0600

    Adding activity, note.

--- app.py
+++ app.py
@@ -2,26 +2,23 @@ from flask import render_template, session, redirect, url_for, request
 from settings import app, db
 from random import choice
 from controller import Controller
-from model import InfoCodes
-modals_colors = (
-    'red lighten-3',
-    'amber lighten-3',
-    'deep-orange accent-1',
-    'blue-grey lighten-1',
-    'amber accent-3',
-    'lime lighten-2',
-    'teal accent-3',
-    'indigo lighten-3',
-    'purple accent-1',
-    'red lighten-2',
-    'light-green lighten-2'
-)
+from model import InfoCodes, MODAL_COLORS, Priorities
+
+
 
 controller = Controller()
 
 
-def get_random_color():
-    return choice(modals_colors)
+def int_to_hours_labels(indx: int) -> str:
+    return f'{"0"*(2 - len(str(indx)))}{indx}:00'
+
+
+def define_schedule() -> list:
+    return controller.get_activities(session['username'])
+
+
+def get_random_color() -> str:
+    return choice(MODAL_COLORS)
 
 
 def render_this_page(url, title):
@@ -39,8 +36,8 @@ def logged_args():
         icon_account = 'person_add'
         redirect_account = 'register'
     else:
-        status_account = 'Profile'
-        icon_account = 'settings'
+        status_account = session['username'][:10]
+        icon_account = 'account_box'
         redirect_account = 'profile'
 
         status_log = 'Logout'
@@ -50,14 +47,10 @@ def logged_args():
     return locals()
 
 
-@app.route('/schedule')
-def schedule():
-    return render_template('schedule.html')
-
-
 @app.route('/test')
 def test():
-    return render_template('home.html')
+    # return render_template('home.html')
+    return render_this_page('404.html', '404')
 
 
 @app.route('/')
@@ -65,7 +58,9 @@ def test():
 @app.route('/home')
 # @logged_args
 def home():
-    url = 'schedule.html' if 'username' in session else 'index.html'
+    if 'username' in session:
+        print(define_schedule())
+    url = 'home.html' if 'username' in session else 'index.html'
     return render_this_page(url, 'HOME')
 
 
@@ -92,6 +87,7 @@ def login():
 @app.route('/register', methods=['POST', 'GET'])
 def register():
     if 'username' in session or request.method == 'GET':
+        print('Entra 2')
         return render_this_page('register.html', 'REGISTER')
     elif request.method == 'POST':
         username = request.form['username']
@@ -110,14 +106,57 @@ def register():
             else:
                 controller.save()
                 return redirect(url_for('home'))
-
+    print('WTH')
     return render_this_page('register.html', 'REGISTER')
+
+
+@app.route('/schedule')
+def schedule():
+    return render_template('schedule.html')
 
 
 @app.route('/logout')
 def logout():
     session.pop('username', None)
     return redirect(url_for('home'))
+
+
+@app.route(('/activity/<string:description>/'
+            '<string:priority>/<string:location>/'
+            '<string:title>/<string:monday>/'
+            '<string:tuesday>/<string:wednesday>/'
+            '<string:thursday>/<string:friday>/'))
+def activity(description, priority=None, location=None,
+             title=None, monday=None, tuesday=None,
+             wednesday=None, thursday=None, friday=None):
+    print(description, priority, location, title)
+    print(monday, tuesday, wednesday, thursday, friday)
+    monday, tuesday, wednesday, thursday, friday = map(lambda s: '' if s == 'None' else s, [monday, tuesday, wednesday, thursday, friday])
+    if 'username' in session:
+        response = controller.add_activity(session['username'], description,
+                                           priority, location, title)
+        if response == InfoCodes.ACTIVITY_ALREADY_EXIST:
+            return redirect(url_for('home'))
+
+        controller.add_schedule(monday, tuesday, wednesday, thursday,
+                                friday, response)
+        controller.save()
+        return redirect(url_for('home'))
+
+    return render_template('404.html'), 404
+
+@app.route('/activity/remove/<string:title>')
+def remove_activity(title):
+    if 'username' in session:
+        if controller.remove_activity(title) == InfoCodes.SUCCESS:
+            controller.save()
+            return redirect(url_for('home'))
+
+    return render_template('404.html'), 404
+
+@app.errorhandler(404)
+def error_404(e):
+    return render_this_page('404.html', '404'), 404
 
 
 if __name__ == '__main__':
