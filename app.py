a1d6b46ea7b79322725ec5392d63a28d88da01ae
HBaena <a.hernandezbaena@ugto.mx>
2019-12-08 21:47:34 -0600

    Adding reminder structure Adding define_notes fucntions that returns the zip with the data that the reminders/html needs to show them up

--- app.py
+++ app.py
@@ -3,8 +3,7 @@ from settings import app, db
 from random import choice
 from controller import Controller
 from model import InfoCodes, MODAL_COLORS, Priorities
-
-
+from datetime import datetime
 
 controller = Controller()
 
@@ -13,16 +12,30 @@ def int_to_hours_labels(indx: int) -> str:
     return f'{"0"*(2 - len(str(indx)))}{indx}:00'
 
 
-def define_schedule() -> list:
-    return controller.get_activities(session['username'])
+def define_notes():
+    response = controller.get_notes(username=session['username'])
+    if response:
+        activities = []
+        for i in range(len(response)):
+            r = controller.get_activity_name(response[i].activity_id)
+            print(r)
+            if r:
+                activities.append(r)
+            else:
+                activities.append('None')
 
+        return zip(response,
+            [get_random_color() for _ in range(len(response))],
+            activities)
+    else:
+        return None
 
-def get_random_color() -> str:
+def get_random_color():
     return choice(MODAL_COLORS)
 
 
-def render_this_page(url, title):
-    kwargs = logged_args()
+def render_this_page(url, title, **kwargs):
+    kwargs = { **logged_args() ,**kwargs }
     return render_template(url, title=title, **kwargs)
 
 
@@ -35,14 +48,16 @@ def logged_args():
         status_account = 'Sign up'
         icon_account = 'person_add'
         redirect_account = 'register'
+        user = None
     else:
         status_account = session['username'][:10]
         icon_account = 'account_box'
-        redirect_account = 'profile'
+        # redirect_account = 'profile'
 
         status_log = 'Logout'
         icon_log = 'exit_to_app'
         redirect_log = 'logout'
+        user = controller.get_user(session['username'])
 
     return locals()
 
@@ -52,16 +67,23 @@ def test():
     # return render_template('home.html')
     return render_this_page('404.html', '404')
 
-
 @app.route('/')
 @app.route('/index')
+def index():
+    return render_this_page('index.html', 'BeePlanner')
+
+# @app.route('/')
 @app.route('/home')
 # @logged_args
 def home():
+    print(controller.get_activity_name(1))
+    notes = None
     if 'username' in session:
-        print(define_schedule())
-    url = 'home.html' if 'username' in session else 'index.html'
-    return render_this_page(url, 'HOME')
+        notes= define_notes()
+        return render_this_page('home.html', 'HOME', notes=notes)
+    else:
+        return redirect(url_for('index'))
+        
 
 
 @app.route('/login', methods=['GET', 'POST'])
@@ -131,7 +153,10 @@ def activity(description, priority=None, location=None,
              wednesday=None, thursday=None, friday=None):
     print(description, priority, location, title)
     print(monday, tuesday, wednesday, thursday, friday)
-    monday, tuesday, wednesday, thursday, friday = map(lambda s: '' if s == 'None' else s, [monday, tuesday, wednesday, thursday, friday])
+    monday, tuesday, wednesday, thursday, friday = map(
+        lambda s: '' if s == 'None' else s, [monday, tuesday,
+                                             wednesday, thursday, 
+                                             friday])
     if 'username' in session:
         response = controller.add_activity(session['username'], description,
                                            priority, location, title)
@@ -145,12 +170,33 @@ def activity(description, priority=None, location=None,
 
     return render_template('404.html'), 404
 
+
 @app.route('/activity/remove/<string:title>')
 def remove_activity(title):
     if 'username' in session:
         if controller.remove_activity(title) == InfoCodes.SUCCESS:
             controller.save()
             return redirect(url_for('home'))
+
+    return render_template('404.html'), 404
+
+
+@app.route('/note/<string:content>/<string:priority>/<string:due_date>/<string:title>')
+def note(content=None, priority=None, due_date=None, title=None):
+    if 'username' in session:
+        creation_date = datetime.today()
+        username = session['username']
+        if due_date != 'None':
+            due_date = datetime.strptime(due_date, '%d-%m-%Y').date()
+        else:
+            due_date = None
+        if title:
+            activity_id = controller.get_activity_id(title)
+        else:
+            activity_id = 0
+        controller.add_note(content, priority, due_date, creation_date, username, activity_id)
+        controller.save()
+        return redirect(url_for('home'))
 
     return render_template('404.html'), 404
 
